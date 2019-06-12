7a6364683d8990ab9836467121e91a13bf3697c5
glm <admin@omnibustechnologies.ca>
2019-06-11 23:25:10 -0400

    Finished parsing loop

--- src/Imagenex852.hpp
+++ src/Imagenex852.hpp
@@ -19,6 +19,17 @@ typedef struct{
 } Imagenex852FileHeader;
 #pragma pack()
 
+#pragma pack(1)
+typedef struct{
+        char            magic[3];
+        uint8_t         headId;
+        uint8_t         serialStatus;
+	uint16_t	reserved1;
+	uint8_t		range;
+	uint16_t	profileRange;
+	uint16_t	dataBytes;
+} Imagenex852ReturnDataHeader;
+#pragma pack()
 
 class Imagenex852{
 	public:
@@ -26,28 +37,59 @@ class Imagenex852{
 			std::ifstream in(filename,std::ios::binary);
 
 			if(in){
-				int bytesToRead = sizeof(Imagenex852FileHeader);
+				Imagenex852FileHeader hdr;
 
-				do{
-					Imagenex852FileHeader hdr;
+				//Read file header
+				while(in.read((char*)&hdr,sizeof(Imagenex852FileHeader))){
+					if(
+						hdr.magic[0]=='8' &&
+						hdr.magic[1]=='5' &&
+						hdr.magic[2]=='2'
+					){
+						printf("Got file header\n");
 
-					if(in.read((char*)&hdr,sizeof(Imagenex852FileHeader))){
-						if(
-							hdr.magic[0]=='8' &&
-							hdr.magic[1]=='5' &&
-							hdr.magic[2]=='2'
-						){
-							std::cerr << "Got file header. Payload is " << hdr.nToReadIndex << " bytes" << std::endl;
-							
+						//TODO: process file header
+
+						Imagenex852ReturnDataHeader returnDataHdr;
+
+						if(in.read((char*)&returnDataHdr,sizeof(Imagenex852ReturnDataHeader))){
+							unsigned int payloadBytes = (hdr.nToReadIndex==0)?0:((hdr.nToReadIndex==2)?252:500);
+							uint8_t	     echoData[501];
+
+							printf("Got return data header\n");
+
+							if(in.read((char*)&echoData,payloadBytes)){
+								//Todo: process payload bytes
+								uint8_t terminationByte;
+
+								printf("Got payload (%d bytes)\n",payloadBytes);
+
+								//read termination byte
+								if(in.read((char*)&terminationByte,1)){
+									if(terminationByte == 0xFC){
+										unsigned int paddingSize = ((hdr.nToReadIndex==0)?127:((hdr.nToReadIndex==2)?383:639)) - payloadBytes - sizeof(Imagenex852FileHeader) - sizeof(Imagenex852ReturnDataHeader)  ;
+
+										if(!in.read((char*)&echoData,paddingSize)){
+											throw new Exception("Error while reading padding\n");
+										}
+									}
+									else{
+										throw new Exception("Bad termination byte found at the end of packet");
+									}
+								}
+								else{
+									throw new Exception("No termination byte found at the end of packet");
+								}
+							}
 						}
 						else{
-							throw new Exception("Invalid 852 file header");
+							throw new Exception("No return data header");
 						}
 					}
 					else{
-						throw new Exception("Error while reader file header");
+						throw new Exception("Invalid 852 file header");
 					}
-				} while(bytesToRead > 0);
+				}
 
 				in.close();
 			}
