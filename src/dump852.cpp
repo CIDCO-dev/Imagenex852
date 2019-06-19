c234c457475993aea1ccaa307f99a739e94b0600
emile <emile@FUNDY.cidco.ca>
2019-06-19 10:54:44 -0400

    Ajout de la fonction ProcessPing dans dump852

--- src/dump852.cpp
+++ src/dump852.cpp
@@ -7,7 +7,38 @@ void printUsage(){
 }
 
 class Dumper852 : public Imagenex852{
-	//TODO
+	void processPing(Imagenex852FileHeader &hdr,Imagenex852ReturnDataHeader & returnDataHdr,uint8_t * echoData,unsigned int payloadBytes){
+                        std::stringstream ss;
+
+			ss << hdr.date << " " << hdr.time << hdr.timeHundredsSeconds << ".dat";
+
+			std::ofstream out(ss.str());
+                        
+                        out << hdr.date << " ";
+                        out << hdr.time;
+                        out << hdr.timeHundredsSeconds << std::endl;
+                        out << unsigned((uint8_t)hdr.mode) << std::endl;
+                        out << unsigned((uint8_t)hdr.startGain) << std::endl;
+                        out << unsigned((uint8_t)hdr.sectorSize) << std::endl;
+                        out << unsigned((uint8_t)hdr.trainAngle) << std::endl;
+                        out << unsigned((uint8_t)hdr.pulseLength) << std::endl;
+                        out << unsigned((uint8_t)hdr.profile) << std::endl;
+                        out << unsigned((uint16_t)hdr.soundSpeed) << std::endl;
+                        out << hdr.userText << std::endl;
+                        out << unsigned((uint8_t)hdr.operatingFrequency) << std::endl;
+                        out << unsigned((uint8_t)hdr.headId) << std::endl;
+
+			printf("Echo data:\n");
+			for(unsigned int i=0;i<payloadBytes;i++){
+				printf("%.2X ",(uint8_t)echoData[i]);
+				out << unsigned((uint8_t)echoData[i]) << " ";
+			}
+
+                        out << std::endl;
+			printf("\n");
+
+			out.close();
+        }
 };
 
 int main(int argc,char **argv){
